30b202ff7bb9a2ccca904edc1913bc2b30cfb8d5
NMoore42 <nicholas.moore5@icloud.com>
2019-06-10 10:00:04 -0500

    Done.

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,11 @@
 function selectionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  let retArr = [];
+  while (arr.length > 0) {
+    let min = Math.min(...arr)
+    let minIndex = arr.indexOf(min)
+    retArr.push(arr.splice(minIndex, 1))
+  }
+  return retArr;
 }
